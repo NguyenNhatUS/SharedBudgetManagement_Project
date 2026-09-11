c3871a165cd2f00b52d8007f48ef088d4f385fee
Nguyen Nhat <nldnhat182006@gmail.com>
2026-09-11 10:38:19 +0700

    initial commit

---
+++ src/main/java/com/nhat/SharedBudgetManagement/SharedBudgetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.nhat.SharedBudgetManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedBudgetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedBudgetManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhat/SharedBudgetManagement/SharedBudgetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhat.SharedBudgetManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedBudgetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
